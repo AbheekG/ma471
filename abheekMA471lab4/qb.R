d32f356748184af40e36f2a843ea9457ac4ce4d1
Abheek Ghosh <abheekghosh.ag@gmail.com>
2017-09-11 15:15:08 +0530

    Submitted Lab 4

--- abheekMA471lab4/qb.R
+++ abheekMA471lab4/qb.R
@@ -6,20 +6,42 @@ n = 1000;
 M = c(20, 50, 100);
 
 for (m in M) {
-	Y = rbinom(m, size = 1, prob = p);
-	X = Y[ sample(1:m, n, replace = TRUE) ];
+	covProb = 0;
+	crossL = 0;
+	crossU = 0;
 
-	p_mle = sum(X)/n;
-	err = -qnorm(alpha/2)*sqrt(p_mle*(1-p_mle)/n);
-	cil = p_mle - err;
-	ciu = p_mle + err;
+	X = rbinom(m*n, size = 1, prob = p);
+	p_mle = sum(X)/(n*m);
+	# print(p_mle); print(length(X))
 
-	if (cil < 0) cil = 0;
-	if (ciu > 1) ciu = 1;
+	for (i in 1:n) {
+		Y = X[((i-1)*m + 1) : (i*m)];
 
-	cat(sprintf("\nBernoulli sample count = %d\np_mle = %f\n", m, p_mle));
-	cat(sprintf("Confidence interval = [%f, %f]\n", cil, ciu));
+		p_mean = mean(Y);
+		# print((i-1)*m); print(p_mean)
+		err = -qnorm(alpha/2)*sqrt(p_mean*(1-p_mean)/m);
+		cil = p_mean - err;
+		ciu = p_mean + err;
 
-	# covProb = sum((qnorm(alpha/2) <= X) & (X <= qnorm(1 - alpha/2)));
-	# cat("Coverage Probability =", covProb/n, "\n")
+		if (cil < 0) {
+			cil = 0;
+			crossL = crossL + 1;
+		}
+
+		if (ciu > 1) {
+			ciu = 1;
+			crossU = crossU + 1;
+		}
+
+		if ((cil <= p_mle) && (p_mle <= ciu)) {
+			covProb = covProb + 1;
+		}
+	}
+	err = -qnorm(alpha/2)*sqrt(p_mle*(1-p_mle)/m);
+
+	cat(sprintf("\nBernoulli sample count = %d\\\\\np_mle = %f\\\\\n", m, p_mle));
+	cat(sprintf("Confidence Interval = [%f, %f]\\\\\n", p_mle-err, p_mle+err));
+	cat(sprintf("Coverage Probability = %f\\\\\n", covProb/n));
+	cat(sprintf("Number of instances of interval LOWER bound going outside parameter space = %d\\\\\n", crossL));
+	cat(sprintf("Number of instances of interval UPPER bound going outside parameter space = %d\\\\\n", crossU));
 }
--- abheekMA471lab4/qi.R
+++ abheekMA471lab4/qi.R
@@ -1,13 +1,13 @@
 rm(list = ls())
 d = read.table("d-csp0108.txt", header=TRUE)
-
+names = c('C', 'SP')
 # Calculating log returns
 for (k in 2:3) {
 	d[,k] = log(1 + d[,k]);
-	cat("Mean =", mean(d[,k]), "\n");
+	cat(names[k-1], "data, Mean =", mean(d[,k]), "\\\\\n");
 }
 
-cat("Yes, mean different from 0.\n")
+cat("Yes, mean different from 0.\\\\\n")
 
 alpha = 0.05;
 n = length(d[,1]);
@@ -20,5 +20,5 @@ for (k in 2:3) {
 	X = d[,k];
 	# covProb = sum((qnorm(alpha/2) <= X) & (X <= qnorm(1 - alpha/2)));
 	covProb = sum( ((X + sig*qnorm(alpha/2))/(1+sig) <= mu) & (mu <= (X + sig*qnorm(1 - alpha/2))/(1+sig)) );
-	cat("Coverage Probability =", covProb/n, "\n")
+	cat(names[k-1], "data, Coverage Probability =", covProb/n, "\\\\\n")
 }
